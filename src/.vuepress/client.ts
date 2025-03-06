8a41c18cf3777bc13e0fd5e55a5532c4d296f0b7
jact <jachavar@gmail.com>
2025-03-06 18:42:34 +0100

    feat: replaced vidstack by vue-lite-youtube-embed

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,9 +1,12 @@
 import { defineClientConfig } from 'vuepress/client'
 
 import TwitterLink from './components/TwitterLink'
+import LiteYoutubeEmbed from 'vue-lite-youtube-embed'
+import 'vue-lite-youtube-embed/style.css'
 
 export default defineClientConfig({
   enhance: ({ app }) => {
     app.component('TwitterLink', TwitterLink)
+    app.component('LiteYoutubeEmbed', LiteYoutubeEmbed)
   },
 })
